e94ff2eba952740c91b01f556172b742fcdf3689
bryan065 <30362590+bryan065@users.noreply.github.com>
2024-08-06 14:37:01 -0400

    Add DZ60-Unicorn KB

---
+++ keyboards/dz60/keymaps/unicorn/rules.mk
@@ -0,0 +1,12 @@
+# Features
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
+
+# Stock keyboard feature disable
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+
+USER_NAME := bryan065
+SRC += \
+	bryan065.c \
+	custom_rgb.c
